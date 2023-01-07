f6ae2b02a9094cf89287479bd64e5c1122f5178d
MariGautham <118919999+MariGautham@users.noreply.github.com>
2023-01-07 16:12:08 +0530

    Update DemoTest.java

--- src/test/java/DemoTest.java
+++ src/test/java/DemoTest.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class DemoTest {
-
+//changes made in brach 1
 	public static void main(String[] args) {
 		WebDriverManager.chromedriver().setup();
 		WebDriver driver=new ChromeDriver();
